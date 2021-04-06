d40dc6a9742d515d1fc8e966ce2966a35b2ea9b7
Nathan Wood <nathanwood@Nathans-Mac-mini.local>
2021-04-05 21:01:15 -0400

    movie page table css

--- client/src/components/moviecontainer/MovieContainer.jsx
+++ client/src/components/moviecontainer/MovieContainer.jsx
@@ -1,24 +1,15 @@
 import Movies from '../movies/Movies.jsx';
 import { Link } from 'react-router-dom';
-import Search from '../search/Search';
 import './MovieContainer.css'
 
 export default function MovieContainer(props) {
-  // const [queriedMovies, setQueriedMovies] = useState([]);
   const { movies, setMovies, handleDelete, currentUser } = props;
 
-  const handleSearch = e => {
-    const results = movies.filter(movie => movie.title.toLowerCase().includes(e.target.value.toLowerCase()));
-    setMovies(results)
-  }
   return (
     <div className='movies-div'>
-      <Search
-        movies={movies}
-        handleSearch={handleSearch}
-      />
       <Movies
         movies={movies}
+        setMovies={setMovies}
       />
       <Link
         to='/movies/new'
--- client/src/components/movies/Movies.jsx
+++ client/src/components/movies/Movies.jsx
@@ -1,21 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Search from '../search/Search';
 import './Movies.css'
 
 export default function Movies(props) {
-  const { movies } = props;
+  const { movies, setMovies } = props;
+
+  const handleSearch = e => {
+    const results = movies.filter(movie => movie.title.toLowerCase().includes(e.target.value.toLowerCase()));
+    setMovies(results)
+  }
   
   return (
     <div>
       <h2 className='page-title'>Movies</h2>
+      <Search
+        className='search'
+        movies={movies}
+        handleSearch={handleSearch}
+      />
+        <div className='movie-info movie-header'>
+          <p className='table-cell'>Title</p>
+          <p className='table-cell'>Release Date</p>
+          <p className='table-cell'>Availability</p>
+          <p className='table-cell'>See More</p>
+        </div>
       <div className='movie-table'>
       {
         movies.map(movie => (
           <div className='movie-info' key={movie.id}>
-              <p>{movie.title}</p>
-              <p>{movie.date}</p>
-              {movie.isAvailable ? <p>&#10003;</p> : <p>-</p>}
-            <Link to={`/movies/${movie.id}`}>View Details</Link>
+              <p className='movie-title table-cell'>{movie.title}</p>
+              <p className='movie-date table-cell'>{movie.date}</p>
+              {movie.isAvailable ? <p className='movie-data table-cell'>&#10003;</p> : <p>-</p>}
+            <Link
+              to={`/movies/${movie.id}`}
+              className='movie-data table-cell'
+            >View Details</Link>
           </div>
         ))
         }
